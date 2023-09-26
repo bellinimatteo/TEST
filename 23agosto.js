a94b0558a89b3fc9fc8b9e57e8d2e9104dc9dd91
bellinimatteo <131378948+bellinimatteo@users.noreply.github.com>
2023-09-26 23:07:14 +0200

    Add files via upload

---
+++ 23agosto.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper('.header-carousel', {
+    // Optional parameters
+   
+    loop: true,
+  
+   
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.header-carousel-next',
+      prevEl: '. header-carousel-prev',
+    },
+  });
+
+  const categories =[
+  {name:"auto", icon:"<i class="fa-solid fa-car-rear></i>,announcementsCount:123};
+  {name:"Elettronica",icon:"<i class="fa-solid fa-laptop></i>,announcementsCount:564};
+  {name:"Moto",icon:"<i class="fa-solid fa-motorcycle ></i>,announcementsCount:230};
+  {name:"Abbigliamento",icon:"<i class="fa-solid fa-shirt></i>,announcementsCount:321};
+  {name:"Sport",icon:"i class="fa-solid fa-person-running ></i>,announcementsCount:90};
+  {name:"Giardinaggio",icon:"i class="fa-solid fa-leaf ></i>,announcementsCount:50};
+  {name:"Casa",icon:"i class="fa-solid fa-house-chimney ></i>,announcementsCount:134};
+  {name:"Cucina",icon:"i class="fa-solid fa-fire-burner ></i>,announcementsCount:176};
+    
+  
+
+  ]
+
+  const categoryWrapper =document.getElementById('CategoryCardsWrapper');
+
+  categories.forEach(function(category) {
+
+  })
+    
+  });
+
